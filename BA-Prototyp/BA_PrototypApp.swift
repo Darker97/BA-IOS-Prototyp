2ea14f0d6fd2a1292a1fdcff90848ed5e8f0b2ce
Christian - Darker97 <christian@baltzer.de>
2020-07-09 11:32:47 +0200

    Initial Commit

---
+++ BA-Prototyp/BA_PrototypApp.swift
@@ -0,0 +1,17 @@
+//
+//  BA_PrototypApp.swift
+//  BA-Prototyp
+//
+//  Created by Christian Baltzer on 09.07.20.
+//
+
+import SwiftUI
+
+@main
+struct BA_PrototypApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BA-Prototyp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BA-Prototyp
+//
+//  Created by Christian Baltzer on 09.07.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
